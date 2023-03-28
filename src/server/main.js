2b18928a8eb41a5eae5c087971320c2730a6849e
Ante Brozović <47977690+aposhtol@users.noreply.github.com>
2023-03-28 03:37:21 +0200

    remove cross-env

--- src/server/main.js
+++ src/server/main.js
@@ -9,10 +9,7 @@ const routes = require('./routes');
 const ViteExpress = require('vite-express');
 
 const app = express();
-//ViteExpress.config({ mode: 'production' });
 
-//app.use(express.static('../'));
-app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 
