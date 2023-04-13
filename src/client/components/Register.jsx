6693df484f83f9d675d2da777b5acce1680ba119
Ante Brozović <47977690+aposhtol@users.noreply.github.com>
2023-04-13 18:34:24 +0200

    add registration and login

---
+++ src/client/components/Register.jsx
@@ -0,0 +1,106 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+//import { setLogin } from '../reducers/userReducer';
+import { register } from '../reducers/userReducer';
+import Notice from './Notification';
+import styled, { keyframes } from 'styled-components';
+import Cogs from '../assets/images/player-cogs.svg';
+//import { setNotification } from '../reducers/notificationReducer';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirm, setConfirm] = useState('');
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const userObj = { username, password, confirm };
+    dispatch(register(userObj));
+    navigate('/');
+    setUsername('');
+    setPassword('');
+    setConfirm('');
+  };
+
+  return (
+    <Container>
+      <Card>
+        <Notice />
+        <h1>Registirajte se</h1>
+        <form onSubmit={handleLogin}>
+          <label htmlFor='Username'>Korisničko ime:</label>
+          <input
+            required
+            id='username'
+            type='text'
+            value={username}
+            name='Username'
+            onChange={({ target }) => setUsername(target.value)}
+          />
+
+          <label htmlFor='Password'>Lozinka:</label>
+          <input
+            required
+            id='password'
+            type='text'
+            value={password}
+            name='Password'
+            onChange={({ target }) => setPassword(target.value)}
+          />
+          <label htmlFor='Confirm'>Lozinka ponovno:</label>
+          <input
+            required
+            id='confirm'
+            type='text'
+            value={confirm}
+            name='Confirm'
+            onChange={({ target }) => setConfirm(target.value)}
+          />
+          <button type='submit'>Registracija</button>
+        </form>
+      </Card>
+    </Container>
+  );
+};
+
+export default Login;
+
+const scaleIn = keyframes`
+  0% {
+    transform: scaleY(0);
+    opacity: 1;
+  }
+  100% {
+    transform: scaleY(1);
+    opacity: 1;
+  }
+`;
+
+const Container = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Card = styled.div`
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+    Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: 5rem;
+  text-shadow: 5px 5px 10px rgba(125, 148, 219, 0.75);
+  color: #1443d5;
+  max-width: 800px;
+  margin: 10rem 2rem;
+  text-align: center;
+  padding: 2rem;
+  background-image: url(${Cogs});
+  box-shadow: 0 8px 16px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+
+  animation: ${scaleIn} 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+`;
